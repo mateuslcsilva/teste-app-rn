9a6a579c4e3b414393c1e2826681a3eaed0a1e13
Mateus <leitemcs@hotmail.com>
2024-01-03 16:36:05 -0300

    tentativa de adicionar som às notificações em segundo plano

--- app/index.js
+++ app/index.js
@@ -10,7 +10,7 @@ import * as Notifications from 'expo-notifications';
 Notifications.setNotificationHandler({
     handleNotification: async () => ({
         shouldShowAlert: true,
-        shouldPlaySound: false,
+        shouldPlaySound: true,
         shouldSetBadge: false,
     }),
 });
